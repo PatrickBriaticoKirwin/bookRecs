dc5d5dd18ee0f1c792cbefa2bc01126a22d38b6c
PatrickBurgerKing <patoy@live.com>
2025-02-19 11:10:54 -0500

    Enable CORS for FE

--- recServer/main.go
+++ recServer/main.go
@@ -5,8 +5,10 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -23,6 +25,15 @@ func main() {
 	defer db.Close()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://patrickbriaticokirwin.github.io"}, // Allow GitHub Pages frontend
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/books", func(c *gin.Context) {
 		recommender := c.Query("recommender")
